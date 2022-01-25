6aa2bbc198b2fde2bc973855434d9365eff44100
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-01-25 13:20:01 +0200

    hw_08_burger-hidden

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,15 @@ let menuBtn = document.querySelector(".menu__btn");
 
 let menu = document.querySelector(".menu");
 
+let mainBlock = document.querySelector(".mainBlock");
+let footerBlock = document.querySelector(".footer");
+let headerBlock = document.querySelector(".headerBlock");
+
 menuBtn.addEventListener("click", function (){
     menuBtn.classList.toggle("active");
     menu.classList.toggle("active");
+
+    mainBlock.classList.toggle("globalHide");
+    footerBlock.classList.toggle("globalHide");
+    headerBlock.classList.toggle("globalHide");
 })
