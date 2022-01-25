9cbc84479772f167abd32349ec37a0e419885eb7
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-01-25 22:39:18 +0200

    hw_08_menu_footer_modal

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
       openModalBtn: document.querySelector('.data-modal-open'),
       closeModalBtn: document.querySelector('.data-modal-close'),
       modal: document.querySelector('.modal'),
+      body: document.body
     };
 
     if (refs.modal) {
@@ -15,18 +16,6 @@
     function toggleModal() {
       refs.modal.style.display = 'flex';
       refs.modal.classList.toggle('is-hidden');
+      refs.body.classList.toggle('lockScroll');
     }
-    // function toggleModal() {
-    //   document.body.classList.toggle ('modal-open');
-    //   refs.modal.classList.toggle('is-hidden');
-    // }
-
-    // menuBtnRef.addEventListener ("click", () => {
-    //   const expended = menuBtnRef.getAttribute ("aria-expended") ==="true" || false;
-    
-    //   menuBtnRef.classList.toggle ("is-open");
-    //   menuBtnRef.setAttribute("aria-expended", !expended);
-
-    //   mobileMenuRef.classList.toggle("is-open");
-    // })
   })();
